8eef0bd7c1110c2c4c0f8f0f4bb11200da512b5c
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-10-30 10:19:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Use splice() to remove elements from 'arr', so that it only contains elements that sum to the value of 10
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1, 4)
+
+console.log(arr);
